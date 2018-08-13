223572c7778aee3762c5188d520c33c2d8ed3fbe
Noelle Rolwes <nrolwes@gmail.com>
2018-08-13 19:48:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 function dwarfRollCall(dwarves) {
+  let dwarfNumberAndName = []
+  for (let i = 0, i<dwarves.length, i++){
+    dwarfNumberAndName.push(` ${i + 1}. ${dwarves[i]} `)
+  }
+    return `${dwarfNameAndNumber.join(' ')}`
 }
 
 function summonCaptainPlanet(planeteerCalls){
